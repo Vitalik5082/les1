daeb1bb5a212123aa48313aca76dbe92fffd6d53
Vitalik5082 <avkvitala@gmail.com>
2022-02-13 14:13:46 +0200

    homework

---
+++ js1/main.js
@@ -0,0 +1,85 @@
+{
+    let greeting = 'hello'
+    console.log(greeting)
+    alert(greeting)
+    document.write("hello ", "owu ", "com ", "ua ")
+    let school = 'owu'
+    console.log(school)
+    alert(school)
+    let indexIntr = 'com'
+    console.log(indexIntr)
+    alert(indexIntr)
+    let indexUkr = 'ua'
+    console.log(indexUkr)
+    alert(indexUkr)
+    let numFirst = 1
+    document.write(1)
+    console.log(numFirst)
+    alert(numFirst)
+    let numTen = 10
+    console.log(numTen)
+    alert(numTen)
+    document.write(10)
+    let numNegative = -999
+    console.log(numNegative)
+    alert(numNegative)
+    document.write(-999)
+    let numPI = 3.14
+    console.log(numPI)
+    alert(numPI)
+    document.write(3.14)
+    let fraction = 2.7
+    console.log(fraction)
+    alert(fraction)
+    document.write(2.7)
+    let numSixteen = 16
+    console.log(numSixteen)
+    alert(numSixteen)
+    document.write(16)
+    let bulianF = Boolean(false)
+    console.log(bulianF)
+    alert(bulianF)
+    document.write(false)
+    let bulianT = Boolean(true)
+    console.log(bulianT)
+    alert(bulianT)
+    document.write(true)
+    greeting = 'hola'
+    school = 'okten'
+    indexIntr = 'in'
+    indexUkr = 'tr'
+    numFirst = 2
+    numTen = 11
+    numNegative = -966
+    numPI = 314
+    fraction = 0
+    numSixteen = 160
+    bulianF = Boolean(!false)
+    bulianT = Boolean(!true)
+    console.log(greeting , school , indexIntr ,indexUkr , numFirst , numPI , numSixteen , numTen , fraction ,bulianT, bulianF)
+}
+let person = {
+    firstName : ("Віталій "),
+    middleName : ("Олександрович "),
+    lastName : ("Ратушняк "),
+    age : 25
+}
+prompt(person.firstName + person.middleName + person.lastName)
+alert("Вітаю " + person.firstName + person.middleName + ". " + "Тобі " + person.age + " років.")
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a ,typeof b ,typeof c,)
+
+// // > < >= <= == === != !==  true false
+console.log(5 < 6)
+console.log(5 > 6)
+console.log(5 >= 6)
+console.log(5 == 6)
+console.log(10 == 10)
+console.log(10 <= 10)
+console.log(10 > 10)
+console.log(10 < 10)
+console.log(10 !== 10)
+console.log(123 === '123')
+console.log(123 == '123
