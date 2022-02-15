f48dc0b729a4e5a4a112762a2c0eb0056356273c
Vitalik5082 <avkvitala@gmail.com>
2022-02-15 17:45:38 +0200

    hw

---
+++ js1/cw/main.js
@@ -0,0 +1,10 @@
+let tenNumber = [1,2,3,4,5,6,7,8,9,10]
+console.log(tenNumber)
+let result = (tenNumber[0]+tenNumber[1]+tenNumber[2]+tenNumber[3]+tenNumber[4]+tenNumber[5]+tenNumber[6]+tenNumber[7]+tenNumber[8]+tenNumber[9])
+console.log(result)
+//2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+let book = {
+    bookName : ("tестирование dOT COM")
+    page : 311
+    genre :  ("IT history")
+}
